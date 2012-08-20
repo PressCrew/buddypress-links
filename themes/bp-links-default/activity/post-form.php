4552ec1e6dc83f6fd01f656bd642eb9c63a28cd1
Marshall Sorenson <marshall@presscrew.com>
2012-08-20 12:50:57 -0400

    remove deprecated mentions message

--- themes/bp-links-default/activity/post-form.php
+++ themes/bp-links-default/activity/post-form.php
@@ -2,12 +2,6 @@
 
 	<?php do_action( 'bp_before_activity_post_form' ) ?>
 
-	<?php if ( isset( $_GET['r'] ) ) : ?>
-		<div id="message" class="info">
-			<p><?php printf( __( 'You are mentioning %s in a new update, this user will be sent a notification of your message.', 'buddypress' ), bp_get_mentioned_user_display_name( $_GET['r'] ) ) ?></p>
-		</div>
-	<?php endif; ?>
-
 	<div id="whats-new-avatar">
 		<a href="<?php echo bp_loggedin_user_domain() ?>">
 			<?php bp_loggedin_user_avatar( 'width=60&height=60' ) ?>
