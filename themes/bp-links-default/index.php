7fb0a27f83911cd7cee7eb2ab2aa135d36042835
Marshall Sorenson <marshall@presscrew.com>
2013-07-23 13:20:36 -0400

    added missing links root component slug to all links tab href

--- themes/bp-links-default/index.php
+++ themes/bp-links-default/index.php
@@ -15,7 +15,7 @@
 
 			<div class="item-list-tabs">
 				<ul>
-					<li class="selected" id="links-all"><a href="<?php bp_root_domain() ?>"><?php _e( 'All Links', 'buddypress-links' ) ?> <span><?php echo bp_get_links_total_link_count() ?></span></a></li>
+					<li class="selected" id="links-all"><a href="<?php bp_root_domain() ?>/<?php bp_root_slug( 'links' ) ?>"><?php _e( 'All Links', 'buddypress-links' ) ?> <span><?php echo bp_get_links_total_link_count() ?></span></a></li>
 					<?php do_action( 'bp_links_directory_link_types' ) ?>
 				</ul>
 			</div><!-- .item-list-tabs -->
